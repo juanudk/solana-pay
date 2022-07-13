bf65bd72e623bc2296ca6218daeb7b7f8b6c82e6
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2022-07-13 14:39:48 -0300

    Add logo community

--- point-of-sale/src/server/api/index.ts
+++ point-of-sale/src/server/api/index.ts
@@ -15,7 +15,7 @@ const get: NextApiHandler<GetResponse> = async (request, response) => {
     if (!label) throw new Error('missing label');
     if (typeof label !== 'string') throw new Error('invalid label');
 
-    const icon = `https://${request.headers.host}/solana-pay-logo.svg`;
+    const icon = `https://${request.headers.host}/LogoCommunity.svg`;
 
     response.status(200).send({
         label,
@@ -34,42 +34,42 @@ const post: NextApiHandler<PostResponse> = async (request, response) => {
     persisted along with an unpredictable opaque ID representing the payment, and the ID be passed to the app client,
     which will include the ID in the transaction request URL. This prevents tampering with the transaction request.
     */
-    const recipientField = request.query.recipient;
-    if (!recipientField) throw new Error('missing recipient');
-    if (typeof recipientField !== 'string') throw new Error('invalid recipient');
-    const recipient = new PublicKey(recipientField);
-
-    const amountField = request.query.amount;
-    if (!amountField) throw new Error('missing amount');
-    if (typeof amountField !== 'string') throw new Error('invalid amount');
-    const amount = new BigNumber(amountField);
-
-    const splTokenField = request.query['spl-token'];
-    if (splTokenField && typeof splTokenField !== 'string') throw new Error('invalid spl-token');
-    const splToken = splTokenField ? new PublicKey(splTokenField) : undefined;
-
-    const referenceField = request.query.reference;
-    if (!referenceField) throw new Error('missing reference');
-    if (typeof referenceField !== 'string') throw new Error('invalid reference');
-    const reference = new PublicKey(referenceField);
-
-    const memoParam = request.query.memo;
-    if (memoParam && typeof memoParam !== 'string') throw new Error('invalid memo');
-    const memo = memoParam || undefined;
-
-    const messageParam = request.query.message;
-    if (messageParam && typeof messageParam !== 'string') throw new Error('invalid message');
-    const message = messageParam || undefined;
-
-    // Account provided in the transaction request body by the wallet.
-    const accountField = request.body?.account;
-    if (!accountField) throw new Error('missing account');
-    if (typeof accountField !== 'string') throw new Error('invalid account');
-    const account = new PublicKey(accountField);
-
-    // Compose a simple transfer transaction to return. In practice, this can be any transaction, and may be signed.
-    let transaction = await createTransfer(connection, account, {
-        recipient,
+   const recipientField = request.query.recipient;
+   if (!recipientField) throw new Error('missing recipient');
+   if (typeof recipientField !== 'string') throw new Error('invalid recipient');
+   const recipient = new PublicKey(recipientField);
+   
+   const amountField = request.query.amount;
+   if (!amountField) throw new Error('missing amount');
+   if (typeof amountField !== 'string') throw new Error('invalid amount');
+   var amount = new BigNumber(amountField);
+   
+   const splTokenField = request.query['spl-token'];
+   if (splTokenField && typeof splTokenField !== 'string') throw new Error('invalid spl-token');
+   const splToken = splTokenField ? new PublicKey(splTokenField) : undefined;
+   
+   const referenceField = request.query.reference;
+   if (!referenceField) throw new Error('missing reference');
+   if (typeof referenceField !== 'string') throw new Error('invalid reference');
+   const reference = new PublicKey(referenceField);
+   
+   const memoParam = request.query.memo;
+   if (memoParam && typeof memoParam !== 'string') throw new Error('invalid memo');
+   const memo = memoParam || undefined;
+   
+   const messageParam = request.query.message;
+   if (messageParam && typeof messageParam !== 'string') throw new Error('invalid message');
+   const message = messageParam || undefined;
+   
+   // Account provided in the transaction request body by the wallet.
+   const accountField = request.body?.account;
+   if (!accountField) throw new Error('missing account');
+   if (typeof accountField !== 'string') throw new Error('invalid account');
+   const account = new PublicKey(accountField);
+   
+   // Compose a simple transfer transaction to return. In practice, this can be any transaction, and may be signed.
+   let transaction = await createTransfer(connection, account, {
+       recipient,
         amount,
         splToken,
         reference,
@@ -82,25 +82,26 @@ const post: NextApiHandler<PostResponse> = async (request, response) => {
             verifySignatures: false,
             requireAllSignatures: false,
         })
-    );
-
-    // Serialize and return the unsigned transaction.
-    const serialized = transaction.serialize({
-        verifySignatures: false,
-        requireAllSignatures: false,
-    });
-    const base64 = serialized.toString('base64');
-
-    response.status(200).send({ transaction: base64, message });
+        );
+        
+        // Serialize and return the unsigned transaction.
+        const serialized = transaction.serialize({
+            verifySignatures: false,
+            requireAllSignatures: false,
+        });
+        const base64 = serialized.toString('base64');
+        
+        console.log(message);
+        response.status(200).send({ transaction: base64, message });
 };
 
 const index: NextApiHandler<GetResponse | PostResponse> = async (request, response) => {
     await cors(request, response);
     await rateLimit(request, response);
-
+    
     if (request.method === 'GET') return get(request, response);
     if (request.method === 'POST') return post(request, response);
-
+    
     throw new Error(`Unexpected method ${request.method}`);
 };
 
